3efe19b126ffb7543910a317ba3b922ae840c9e3
Nelson drew <nelsondrew07@gmail.com>
2024-12-08 12:22:08 +0530

    git ignore and env loading for backend

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"golang-gin-boilerplate/internal/routes"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println("Loaded env")
 	// Initialize and start the server
 	router := routes.SetupRouter()
 
